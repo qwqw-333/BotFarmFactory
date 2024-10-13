dc51f722346ac071ca3b1c835ba529b38293d7ad
konoval <qwqw-3330512@gmail.com>
2024-10-13 09:53:32 +0300

    Delete bots

--- bots/solstone/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import timezone 
-import datetime 
-  
-def utc_timestamp():  
-    now = datetime.datetime.now(timezone.utc) 
-    utc_time = now.replace(tzinfo=timezone.utc) 
-    return utc_time.timestamp()
-  
